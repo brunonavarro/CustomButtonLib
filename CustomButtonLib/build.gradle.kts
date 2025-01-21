231b721bfa4730332268dbc23493df13f66e8683
Bruno Navarro <brunonavarro34@gmail.com>
2025-01-20 19:08:46 -0500

    second commit for publish CustomButtonLib - beta6

--- CustomButtonLib/build.gradle.kts
+++ CustomButtonLib/build.gradle.kts
@@ -22,7 +22,7 @@ android {
                     from(components["release"])
                     groupId = "com.github.brunonavarro"
                     artifactId = "CustomButtonLib"
-                    version = "1.0.0-beta5"
+                    version = "1.0.0-beta6"
                 }
                 repositories {
                     mavenLocal()
