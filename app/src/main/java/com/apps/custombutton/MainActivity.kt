2e1ec909bac4e183a446aef2657374c14f6e3f8d
JorgeNavarroN <jnnavarroxd@gmail.com>
2025-07-17 11:39:39 -0500

    experimental BOM for publish CustomButtonLib v9.2025-07-16

--- app/src/main/java/com/apps/custombutton/MainActivity.kt
+++ app/src/main/java/com/apps/custombutton/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.apps.custombuttonlib.components.CustomButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
